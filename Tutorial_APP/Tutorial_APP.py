2e136a74b803f15e06cd75ec32d1958dacb74121
Bartosz Jagiełło <vixeedv7@gmail.com>
2022-03-25 16:02:58 +0100

    Dodanie możliwości wybrania rozdziału

--- Tutorial_APP/Tutorial_APP.py
+++ Tutorial_APP/Tutorial_APP.py
@@ -3,10 +3,12 @@ import math #rozszerzona matematyka
 import tkinter #graficzne GUI
 import random #moduł pozwalający tworzyć liczby pseudolosowe 
 
-wartosc1=int(input('Wpisz 1. liczbe '))
-#Dzięki poleceniu "input możemy podać wartość zmiennej podczas działania programu"
-wartosc2=int(random.randint(-10, 10))
-#losujemy liczbę od -10 do 10 używając modułu random.randint(-10, 10)
-print(wartosc1)#Drukujemy wartość pierwszej zmiennej
-print(wartosc2)#Drukkujemy wartość drugiej zmiennej
-print(wartosc1*wartosc2)#Drukujemy iloczyn naszych zmienncy
+rozdzial=int(input("Wpisz który rozdiał chcesz przetestować: "))
+if rozdzial == 1:
+    wartosc1=int(input('Wpisz 1. liczbe '))
+    #Dzięki poleceniu "input możemy podać wartość zmiennej podczas działania programu"
+    wartosc2=int(random.randint(-10, 10))
+    #losujemy liczbę od -10 do 10 używając modułu random.randint(-10, 10)
+    print(wartosc1)#Drukujemy wartość pierwszej zmiennej
+    print(wartosc2)#Drukkujemy wartość drugiej zmiennej
+    print(wartosc1*wartosc2)#Drukujemy iloczyn naszych zmienncy
