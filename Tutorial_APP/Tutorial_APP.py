eb2d92f669daf261abbe23a8ff50115184ac1a9a
Bartosz Jagiełło <vixeedv7@gmail.com>
2022-03-25 16:28:46 +0100

    importowanie dodatkowego modułu(kalkulator)

--- Tutorial_APP/Tutorial_APP.py
+++ Tutorial_APP/Tutorial_APP.py
@@ -1,13 +1,16 @@
 #importowanie przykładowych rozszerzeń
 import math #rozszerzona matematyka
 import tkinter #graficzne GUI
-import random #moduł pozwalający tworzyć liczby pseudolosowe 
+import random #moduł pozwalający tworzyć liczby pseudolosowe
+import kalkulator as kalk #importowanie podprogramu
 
 print("Program do wyboru:")
 print("1. Kalkulator")
 
 rozdzial=int(input("Wpisz nr. rozdiału króry chcesz przetestować: "))
 if rozdzial == 1:
+    kalk.licz()
+elif rozdzial == 2:
     wartosc1=int(input('Wpisz 1. liczbe '))
     #Dzięki poleceniu "input możemy podać wartość zmiennej podczas działania programu"
     wartosc2=int(random.randint(-10, 10))
@@ -15,4 +18,3 @@ if rozdzial == 1:
     print(wartosc1)#Drukujemy wartość pierwszej zmiennej
     print(wartosc2)#Drukkujemy wartość drugiej zmiennej
     print(wartosc1*wartosc2)#Drukujemy iloczyn naszych zmienncy
-elif rozdzial == 2:
